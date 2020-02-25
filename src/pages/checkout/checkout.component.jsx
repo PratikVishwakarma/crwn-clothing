8cbc0113eef1f3d034bd8baea8c11e59e556cd71
northout-pratik <Northout123>
2020-02-25 12:47:40 +0530

    added add/remove single item functionality to the checkcout page on checkout item component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -6,37 +6,45 @@ import { connect } from 'react-redux'
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
 import { createStructuredSelector } from 'reselect'
+import CartItem from '../../components/cart-item/cart-item.component'
 
 
-const CheckoutPage = ({ cartItems, total }) => (
-    <div className='checkout-page'>
-        <div className='checkout-header'>
-            <div className='header-block'>
-                <span>Product</span>
+const CheckoutPage = ({ cartItems, total }) => {
+    return cartItems.length ? (
+        <div className='checkout-page'>
+            <div className='checkout-header'>
+                <div className='header-block'>
+                    <span>Product</span>
+                </div>
+                <div className='header-block'>
+                    <span>Description</span>
+                </div>
+                <div className='header-block'>
+                    <span>Quantity</span>
+                </div>
+                <div className='header-block'>
+                    <span>Price</span>
+                </div>
+                <div className='header-block'>
+                    <span>Remove</span>
+                </div>
             </div>
-            <div className='header-block'>
-                <span>Description</span>
-            </div>
-            <div className='header-block'>
-                <span>Quantity</span>
-            </div>
-            <div className='header-block'>
-                <span>Price</span>
-            </div>
-            <div className='header-block'>
-                <span>Remove</span>
-            </div>
-        </div>
-        {
-            cartItems.map(cartItem =>
-                <CheckoutItem key = {cartItem.id} cartItem={cartItem}/>
+            {
+                cartItems.map(cartItem =>
+                    <CheckoutItem key={cartItem.id} cartItem={cartItem} />
                 )
-        }
-        <div className='total'>
-            <span>Total:${total}</span>
+            }
+            <div className='total'>
+                <span>Total:${total}</span>
+            </div>
         </div>
-    </div>
-)
+    )
+        : (<div className='checkout-page'>
+            <span className='empty-message'>Your cart is empty</span>
+        </div>   
+        )
+
+}
 
 
 export const mapStateToProp = createStructuredSelector({
